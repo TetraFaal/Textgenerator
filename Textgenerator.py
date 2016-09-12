f0838ca8945b88d0fbadb7f040d2331da8a7452b
Tetra <rogi.1107@live.com>
2016-09-12 11:21:32 +0200

    Textgenerator

--- Textgenerator.py
+++ Textgenerator.py
@@ -1,1 +1,23 @@
-from random import randint
+import cherrypy
+import os.path
+
+media = os.path.join(os.path.abspath("."), u'GUI')
+
+class HelloWorld(object):
+
+    def index(self):
+        return open(os.path.join(media, u'HtmlTextgenerator.html'))
+    index.exposed = True
+
+    def start(self, text=None, length=None):
+        print(text)
+        return
+    start.exposed = True
+
+class Markov(object):
+
+    def getText(self):
+
+
+configfile = os.path.join(os.path.dirname(__file__),'serv.conf')
+cherrypy.quickstart(HelloWorld(),config=configfile)
