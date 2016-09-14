244e2bff3a4f2289c3d95a0865d8b08451387dbf
Chiiper <chiiperboblar@hotmail.fr>
2016-09-14 10:20:53 +0200

    Javascript button doesn't load new page

--- GUI/JsTextgenerator.js
+++ GUI/JsTextgenerator.js
@@ -1,7 +1,16 @@
-window.onload="ready()";
-
-function ready() {
-	document.getElementById("text").onchange = function() {
-		
-	}
+function FbuttonGenerate(event){
+	event.preventDefault()
+	
+	var textInput = document.getElementsByName("textFromHtml")[0].value;
+	
+	fetch('http://127.0.0.1:8080/start?textFromHtml=' + encodeURIComponent(textInput))
+    .then(function(response) {
+		response.json().then(function (json){
+			console.debug(json);
+			document.getElementById('result').innerHTML=json;
+		})        
+    })
 }
+
+
+
