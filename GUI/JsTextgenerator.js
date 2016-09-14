b77916e93dd02b6bba126e45caaea98d77df1b0a
Chiiper <chiiperboblar@hotmail.fr>
2016-09-14 13:08:20 +0200

    Added elements to GUI
    Added variable length, but didn't join it to server

--- GUI/JsTextgenerator.js
+++ GUI/JsTextgenerator.js
@@ -1,14 +1,21 @@
 function FbuttonGenerate(event){
 	event.preventDefault()
-	console.debug("called")
 	
 	var textInput = document.getElementsByName("textFromHtml")[0].value;
+	var lengthInput = document.getElementsByName("lenght")[0].value; 
 	
 	fetch('http://127.0.0.1:8080/start?textFromHtml=' + encodeURIComponent(textInput))
     .then(function(response) {
 		response.json().then(function (json){
+			
+			// for( var i=0; i<json.length; i++ )
+				// {
+					// document.write(json[i] + '<br />');
+				// }
+
 			console.debug(json);
 			document.getElementById('result').innerHTML=json;
+			document.getElementById('length').innerHTML=lengthInput;
 		})        
     })
 }
