36e580ada2d0eb79a98ff74adc57379d9a7c7d5b
Chiiper <chiiperboblar@hotmail.fr>
2016-09-14 10:32:40 +0200

    Revert "Javascript button doesn't load new page"
    This reverts commit 244e2bff3a4f2289c3d95a0865d8b08451387dbf.

--- GUI/JsTextgenerator.js
+++ GUI/JsTextgenerator.js
@@ -1,16 +1,7 @@
-function FbuttonGenerate(event){
-	event.preventDefault()
-	
-	var textInput = document.getElementsByName("textFromHtml")[0].value;
-	
-	fetch('http://127.0.0.1:8080/start?textFromHtml=' + encodeURIComponent(textInput))
-    .then(function(response) {
-		response.json().then(function (json){
-			console.debug(json);
-			document.getElementById('result').innerHTML=json;
-		})        
-    })
+window.onload="ready()";
+
+function ready() {
+	document.getElementById("text").onchange = function() {
+		
+	}
 }
-
-
-
